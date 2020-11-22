fdf722d7a32dc8819dcce086627ec0c598bb7060
Varun Arora <varora@clemson.edu>
2020-11-22 17:52:41 -0500

    hopefully done

--- project3/hw3.pl
+++ project3/hw3.pl
@@ -1,41 +1,36 @@
 %facts
 male(adam).
 female(mona).
-
 male(clancy).
 female(jackie).
-
 female(haana).
 male(homer).
-
 female(marge).
 male(peter).
 female(selma).
-
 male(bart).
 female(lisa).
 female(maggie).
-
 male(ling).
-
-father(clancy, marge).
-father(clancy, peter).
-father(clancy, selma).
-mother(jackie, marge).
-mother(jackie, peter).
-mother(jackie, selma).
-
-father(homer, bart).
-father(homer, lisa).
-father(homer, maggie).
-mother(marge, bart).
-mother(marge, lisa).
-mother(marge, maggie).
-
-mother(selma, ling)
-
-
-
+married(jackie,clancy).
+married(clancy,jackie).
+married(homer,marge).
+married(marge,homer).
+married(adam,mona).
+married(mona,adam).
+father(clancy,marge).
+father(clancy,peter).
+father(clancy,selma).
+mother(jackie,marge).
+mother(jackie,peter).
+mother(jackie,selma).
+father(homer,bart).
+father(homer,lisa).
+father(homer,maggie).
+mother(marge,bart).
+mother(marge,lisa).
+mother(marge,maggie).
+mother(selma,ling).
 %rules
 parent(X,Y) :- child(X,Y).
 child(X,Y) :- parent(Y, X). 
